d1c9606e08fa80d102faee0653a4017953392adb
Ulug'bek <ulugbekrozimboyev@gmail.com>
2021-09-15 01:10:31 +0500

    discovery - control connections between rest services

--- rest-1/src/main/kotlin/uz/ulugbek/springcloud/rest1/Rest1Application.kt
+++ rest-1/src/main/kotlin/uz/ulugbek/springcloud/rest1/Rest1Application.kt
@@ -2,15 +2,19 @@ package uz.ulugbek.springcloud.rest1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 
+@EnableEurekaClient
 @SpringBootApplication
 class Rest1Application {
 
     @Bean
+    @LoadBalanced
     fun restTemplate() = RestTemplate();
 }
 
@@ -23,7 +27,7 @@ class MyRestController(val restTemplate: RestTemplate) {
 
     @GetMapping
     fun hello() : String {
-        return "Hello from demo 1 " + restTemplate.getForObject("http://localhost:8082", String::class.java)
+        return "Hello from demo 1 " + restTemplate.getForObject("http://rest2", String::class.java)
     }
 
 }
--- rest-2/src/main/kotlin/uz/ulugbek/springcloud/rest2/Rest2Application.kt
+++ rest-2/src/main/kotlin/uz/ulugbek/springcloud/rest2/Rest2Application.kt
@@ -2,9 +2,11 @@ package uz.ulugbek.springcloud.rest2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@EnableEurekaClient
 @SpringBootApplication
 class Rest2Application
 
